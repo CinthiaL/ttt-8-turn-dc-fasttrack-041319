243d1dded3ae8194481c817d3dee0e399e648e72
CL <cl3518@columbia.edu>
2019-04-14 15:47:43 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -22,7 +22,20 @@ def valid_move?
   if index is between (0,9) && 
     
 #position_taken?
+# code your #position_taken? method here!
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return FALSE
+  else 
+    return TRUE
+  end
+end
 
+def valid_move?(board, index)
+  if index.between(0,8) && !position_taken(board, index) 
+    return TRUE
+  else FALSE
+end
 
 #move 
 # code your input_to_index and move method here!
